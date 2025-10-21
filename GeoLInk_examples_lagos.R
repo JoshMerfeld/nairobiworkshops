c952fa05afbb9900e2dc4bcfbfd3ff80a7eae116
davidn10 <david.newhouse@gmail.com>
2025-10-21 12:07:59 +0300

    Updates

--- GeoLInk_examples_lagos.R
+++ GeoLInk_examples_lagos.R
@@ -5,8 +5,7 @@ library(devtools)
 library(tidyverse)
 library(sf)
 # install from github 
-devtools::install_github("SSA-Statistical-Team-Projects/GeoLink")
-
+#devtools::install_github("SSA-Statistical-Team-Projects/GeoLink")
 # Local install as a backup 
 #install.packages("C:/Users/wb200957/OneDrive - WBG/DEC/Github projects/GeoLink", repos = NULL, type = "source")
 
@@ -31,6 +30,7 @@ population_grid <- population_grid |>
   rename(population = geolink_feature)
 
 grid <- grid[population_grid$population>0,]
+population_grid <- population_grid[population_grid$population>0,]
 saveRDS(grid,file="grid")
 grid <- readRDS("grid")
 
@@ -55,6 +55,12 @@ geolink_ndvi_grid <- geolink_vegindex(
   start_date="2022-01-01",
   end_date="2022-12-31"
 ) 
+
+#Take mean ndvi across monthly values 
+geolink_ndvi_grid <- st_drop_geometry(geolink_ndvi_grid)
+indices <- grepl(pattern = "^ndvi_y2022_m*", x=colnames(geolink_ndvi_grid)) 
+geolink_ndvi_grid[,"ndvi_y2022"]  <- rowMeans(geolink_ndvi_grid[,indices])
+geolink_ndvi_grid <- geolink_ndvi_grid[,!indices]
 
 #7. Pollution 
 geolink_pollution_o3_grid <- geolink_pollution(
@@ -91,3 +97,22 @@ geolink_land_grid <- geolink_landcover(
   ) 
 
 
+
+
+
+#11. Putting it all together 
+data <- list(population_grid,ntl_grid,geolink_buildings_grid,geolink_ndvi_grid,geolink_pollution_o3_grid,geolink_cell_grid,geolink_land_grid)
+
+for (x in data[data!="population_grid"]) {
+  x <- st_drop_geometry(x)
+}
+population_grid <- select(population_grid,c("poly_id","ADM2_EN","ADM2_PCODE","population"))
+ntl_grid <- select(ntl_grid,"ntl_mean") 
+geolink_buildings_grid <- select(geolink_buildings_grid,"count":"urban")
+geolink_ndvi_grid <- select(geolink_ndvi_grid,"ndvi_y2022")
+geolink_pollution_o3_grid <- select(geolink_pollution_o3_grid,"o3_y2022")
+geolink_cell_gid <- select(geolink_cell_grid,c("cell_towers","distance_nearest_tower"))
+geolink_land_grid <- select(geolink_land_grid,"water":"rangeland")
+
+features <- do.call(cbind,data)
+saveRDS(features,file="features")
